5c275c8b60f4d5e45da9b9d474e08564fec3a260
Derrick Munyole <derrickjust@gmail.com>
2026-08-06 10:46:05 +0300

    chore: Reformat caserepository

--- src/main/java/com/example/casemanagementplatform/cases/CaseRepository.java
+++ src/main/java/com/example/casemanagementplatform/cases/CaseRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface CaseRepository  extends JpaRepository<Case, Long> {
+public interface CaseRepository extends JpaRepository<Case, Long> {
     List<Case> findByStatus(Case.CaseStatus status);
+
     Optional<Case> findByIdAndTenantId(long id, String tenantId);
+
     List<Case> findAllByTenantId(String tenantId);
 }
